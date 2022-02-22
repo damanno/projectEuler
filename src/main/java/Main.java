942b2b665610d8dc15199a3224a5468d78d4a7f6
damanno <damanno@coderp.sp.gov.br>
2022-02-22 17:01:30 -0300

    Problem 12 Highly divisible triangular number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+
+import java.math.BigInteger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,11 +12,11 @@
  * @author damanno
  */
 public class Main {
-    
+
     public static double total = 0d;
     public static long totalL = 0l;
-    
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         /*
         total = calculaRendimento(0, 4, 0.5, 100);
         total = multiples3or5(1000);
@@ -23,10 +26,15 @@ public class Main {
         totalL = smallestDivisibleNumberByAllNumbers(1, 20);
         totalL = differenceSumSquareSquareSum(100);
         totalL = primeNumberPosition(10001);
-        System.out.println("total " + totalL);
-        */
+        totalL = Problem9.pythagoreanTriplet(1000);
+        totalL = Problem10.summationPrimes(2_000_000);
+         */
         
-    }   
-     
-     
+        totalL = Problem12.highlyDivisibleTriangularNumber(5);
+
+        System.out.println("total " + total);
+        System.out.println("totalL " + totalL);
+
+    }
+
 }
---
+++ src/main/java/Problem12.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import util.UtilMethods;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author damanno
+ */
+public class Problem12 {
+
+    public static long highlyDivisibleTriangularNumber(int maxDivisor) {
+
+        List<Integer> sequence = new ArrayList<>();
+        long result = 0;
+        boolean continua = true;
+        int valor = 0;
+        int iterator = 1;
+
+        while (continua) {
+
+            valor += iterator;
+
+            int divisor = 2;
+            while (divisor <= valor) {
+                if ( valor % divisor == 0) {
+                    sequence.add(divisor);
+                } 
+                    divisor++;
+                
+            }
+            sequence.add(1);
+
+            if (maxDivisor <= sequence.size()) {
+                continua = false;
+            }
+            iterator++;
+            sequence.clear();
+
+        }
+
+        return result;
+
+    }
+
+}
