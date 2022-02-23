7085bf9ff5e0e2226b54a5becdd2be0d0ab91602
damanno <damanno@coderp.sp.gov.br>
2022-02-23 10:58:12 -0300

    Problem 14 solution largest number that produces the largest collatz chain

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,9 +31,10 @@ public class Main {
         totalL = Problem9.pythagoreanTriplet(1000);
         totalL = Problem10.summationPrimes(2_000_000);
         totalL = Problem12.highlyDivisibleTriangularNumber(5);
+        totalS = Problem013.largeSum(10);
         */
         
-        totalS = Problem013.largeSum(10);
+        totalBI = Problem014.returnNumberProducesLargestCollatzChain(1000000);
 
         System.out.println("total " + total);
         System.out.println("totalL " + totalL);
